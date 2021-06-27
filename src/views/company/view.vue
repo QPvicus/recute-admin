cb492f6f2f38c0f20389d97fa233ae177bf2e2a5
QPvicus <57519939+QPvicus@users.noreply.github.com>
2021-06-27 16:02:45 +0800

    aa

--- src/views/company/view.vue
+++ src/views/company/view.vue
@@ -1,13 +1,11 @@
 <!--
  * @Author: Taylor Swift
  * @Date: 2021-06-27 10:57:31
- * @LastEditTime: 2021-06-27 15:03:59
+ * @LastEditTime: 2021-06-27 16:01:45
  * @Description:
 -->
 
-<template>
-  <h1>Hello world This is vv</h1>
-</template>
+<template>d</template>
 
 <script lang="ts">
 import { defineComponent } from 'vue'
